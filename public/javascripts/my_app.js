ac022b86e06475011a6b1b87dad7de0f893e04a7
Ubuntu <ashley.hirst425@gmail.com>
2017-12-13 14:46:20 +0000

    Final working project

--- public/javascripts/my_app.js
+++ public/javascripts/my_app.js
@@ -1,13 +1,25 @@
-angular.module('myApp', []).
-  controller('myController', ['$scope', '$http',
-                              function($scope, $http) {
+angular.module('myApp', ['firebase' ]).
+  controller('myController', ['$scope', '$http', '$firebaseArray',
+                              function($scope, $http, $firebaseArray) {
     $http.get('/user/profile')
         .success(function(data, status, headers, config) {
       $scope.user = data;
+      console.log('user: ' + $scope.user);
       $scope.error = "";
     }).
     error(function(data, status, headers, config) {
       $scope.user = {};
       $scope.error = data;
     });
+    var ref = firebase.database().ref().child("messages");
+   $scope.chats = $firebaseArray(ref);
+     console.log($scope.chats);
+     $scope.update = function(user) {
+       console.log("name:" + $scope.user.username);
+       var newmessage = {from: $scope.user.username || "anonymous",body:user.chat};
+       console.log(newmessage);
+       $scope.chats.$add(newmessage);
+       user.chat = "";
+   }
   }]);
+
